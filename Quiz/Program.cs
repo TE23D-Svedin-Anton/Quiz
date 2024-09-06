54eb68e4fce7beb0da46fc133721a8330ed20ac9
Anton Svedin <anton.svedin@elev.ga.ntig.se>
2024-09-06 10:22:01 +0200

    Ställer frågorna

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Poäng;
+string val;
+
+Console.WriteLine("Välkommen till min frågesport. Du kommer få 3 kluriga frågor att svara på. Lycka till!");
+Console.ReadLine();
+
+Console.WriteLine("Fråga nummer 1,");
+val = Console.ReadLine();
+if (val.ToLower() == "")
+
+Console.WriteLine("Fråga nummer 2,");
+val = Console.ReadLine();
+if (val.ToLower() == "")
+
+Console.WriteLine("Fråga nummer 3,");
+
+if (val.ToLower() == "")
+
+Console.WriteLine("");
+Console.ReadLine();
